a769a499a08f49f2eaf6738eea63d9bc967c4c4f
Dominik.Kotecki <dominikus1910@gmail.com>
2020-04-02 21:37:47 +0200

    first solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+
+fn rem(a: i32, b:i32) -> i32 {
+    return a - (b * (a/b))
+}
+
+fn project_euler_1() -> i32 {
+
+    return (0..1000).filter(|&x| rem(x, 5) == 0 || rem(x, 3) == 0).sum();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = project_euler_1();
+    println!("Hello, world! {:?}", a);
 }
